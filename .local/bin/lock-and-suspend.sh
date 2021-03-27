ba556b889c6b5ca6d9ab2606a69b1a56177304a5
mongwell <19lion@gmail.com>
2021-03-26 19:50:59 -0500

    removed old unneeded configs

--- .local/bin/lock-and-suspend.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-./pixelated-lock.sh
-systemctl suspend
-
--- .local/bin/touchpad.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if xinput list-props 16 | grep "Device Enabled (139):	1" ; then
-    xinput disable 16 ;
-else
-    xinput enable 16 ;
-fi
