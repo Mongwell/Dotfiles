49f3417c576b5fd030aebde321387e53c16e16b5
mongwell <19lion@gmail.com>
2022-06-17 04:28:15 -0500

    add keymaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,2 @@
 require "mongwell.options"
+require "mongwell.keymaps"
---
+++ .config/nvim/lua/mongwell/keymaps.lua
@@ -0,0 +1,61 @@
+local opts = { noremap = true, silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+-- Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Lex > Sex
+keymap("n", "<leader>o", ":Lexplore<CR>", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Navigate buffers
+keymap("n", "<leader>bl", ":bnext<CR>", opts)
+keymap("n", "<leader>bh", ":bprevious<CR>", opts)
+
+-- Clear search
+keymap("n", "<leader>l", ":noh<CR>", opts)
+
+-- Scroll bind splits
+keymap("n", "<leader>vl", ":set scrollbind<CR>", opts)
+keymap("n", "<leader>vu", ":set noscrollbind<CR>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+
+-- Don't yank to register when pasting in visual mode
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
