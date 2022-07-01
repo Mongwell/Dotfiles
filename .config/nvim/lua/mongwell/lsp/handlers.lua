03db7047097009751655d0f3fd34dfa849609f56
mongwell <19lion@gmail.com>
2022-06-30 20:41:48 -0500

    improve formatting binding

--- .config/nvim/lua/mongwell/lsp/handlers.lua
+++ .config/nvim/lua/mongwell/lsp/handlers.lua
@@ -74,7 +74,14 @@ local function lsp_keymaps(bufnr)
     vim.api.nvim_buf_set_keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
     vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
     vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-    vim.cmd [[ command! Format execute 'lua vim.lsp.buf.format()' ]]
+    vim.cmd [[
+        fun! Format()
+            let l:save = winsaveview()
+            keeppatterns %s/\s\+$//e      
+            call winrestview(l:save)
+            execute 'lua vim.lsp.buf.format()'
+        endfun
+    ]]
 end
 
 M.on_attach = function(client, bufnr)
