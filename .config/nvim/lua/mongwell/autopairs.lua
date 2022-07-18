7892fbce41668467b1570e4715592803b8bb872a
mongwell <19lion@gmail.com>
2022-07-17 19:58:33 -0500

    autopairs pair inside spaces

--- .config/nvim/lua/mongwell/autopairs.lua
+++ .config/nvim/lua/mongwell/autopairs.lua
@@ -2,6 +2,7 @@ local status_ok, autopairs = pcall(require, "nvim-autopairs")
 if not status_ok then
     return
 end
+local Rule = require("nvim-autopairs.rule")
 
 autopairs.setup {
     disable_filetype = { "TelescopePrompt" },
@@ -31,3 +32,29 @@ cmp.event:on(
     "confirm_done",
     cmp_autopairs.on_confirm_done()
 )
+
+autopairs.add_rules {
+    Rule(' ', ' ')
+        :with_pair(function(opts)
+            local pair = opts.line:sub(opts.col - 1, opts.col)
+            return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+        end),
+    Rule('( ', ' )')
+        :with_pair(function() return false end)
+        :with_move(function(opts)
+            return opts.prev_char:match('.%)') ~= nil
+        end)
+        :use_key(')'),
+    Rule('{ ', ' }')
+        :with_pair(function() return false end)
+        :with_move(function(opts)
+            return opts.prev_char:match('.%}') ~= nil
+        end)
+        :use_key('}'),
+    Rule('[ ', ' ]')
+        :with_pair(function() return false end)
+        :with_move(function(opts)
+            return opts.prev_char:match('.%]') ~= nil
+        end)
+        :use_key(']')
+}
