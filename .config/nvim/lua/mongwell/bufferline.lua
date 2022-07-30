4a88316e579a6749063df892d748c09112041231
mongwell <19lion@gmail.com>
2022-07-29 20:15:00 -0500

    bufferline settings tweaks

--- .config/nvim/lua/mongwell/bufferline.lua
+++ .config/nvim/lua/mongwell/bufferline.lua
@@ -49,7 +49,7 @@ bufferline.setup {
 
         max_name_length = 30,
         max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
-        tab_size = 15,
+        tab_size = 20,
         diagnostics = false,
         diagnostics_update_in_insert = false,
         -- The diagnostics indicator can be set to nil to keep the buffer name highlight but delete the highlighting
@@ -63,10 +63,10 @@ bufferline.setup {
         } },
         color_icons = true,
         show_buffer_icons = true,
-        show_buffer_close_icons = false,
+        show_buffer_close_icons = true,
         show_buffer_default_icon = false,
         show_close_icon = false,
-        show_tab_indicators = false,
+        show_tab_indicators = true,
         separator_style = "slant", -- "slant" | "thick" | "thin" | { 'any', 'any' },
         enforce_regular_tabs = true,
     },
