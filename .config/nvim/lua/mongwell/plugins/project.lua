bbcb44465de924c0d4d80e83dbef16e6dfd38df3
mongwell <19lion@gmail.com>
2023-07-29 05:12:17 -0500

    add project config

---
+++ .config/nvim/lua/mongwell/plugins/project.lua
@@ -0,0 +1,20 @@
+config_opts = {
+	manual_mode = false,
+	detection_methods = { "lsp", "pattern" },
+	patterns = { "=orgspace" },
+	exclude_dirs = {},
+}
+
+return {
+	"telescope.nvim",
+	dependencies = {
+		"ahmedkhalf/project.nvim",
+		lazy = true,
+		opts = config_opts,
+		cmd = "Telescope projects",
+		config = function(_, opts)
+			require("project_nvim").setup(opts)
+			require("telescope").load_extension("projects")
+		end,
+	},
+}
