46233e4bbea7acb10f25ed36e6f3e13d327e5fe5
mongwell <19lion@gmail.com>
2023-07-30 18:40:35 -0500

    setup lazy treesitter

---
+++ .config/nvim/lua/mongwell/plugins/treesitter.lua
@@ -0,0 +1,66 @@
+local ts_opts = {
+    ensure_installed = {
+        "bash",
+        "c",
+        "comment",
+        "cmake",
+        "cpp",
+        "diff",
+        "dockerfile",
+        "git_config",
+        "git_rebase",
+        "gitattributes",
+        "gitcommit",
+        "gitignore",
+        "go",
+        "json",
+        "lua",
+        "luadoc",
+        "make",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "regex",
+        "rust",
+        "toml",
+        "yaml",
+    },
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+        enable = true,
+        use_languagetree = true,
+        disable = { "" },
+        additional_vim_regex_highlighting = { "org" },
+    },
+    autopairs = {
+        enable = true,
+    },
+    indent = {
+        enable = true,
+        disable = { "python", "org", "yaml" },
+    },
+}
+
+
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    lazy = true,
+    cmd = {
+        "TSInstall",
+        "TSUninstall",
+        "TSUpdate",
+        "TSUpdateSync",
+        "TSInstallInfo",
+        "TSInstallSync",
+        "TSInstallFromGrammar",
+    },
+    event = { "BufReadPost", "BufNewFile" },
+    opts = ts_opts,
+    init = function()
+        vim.o.foldmethod = "expr"
+        vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+        vim.o.foldenable = 0
+    end
+}
--- .config/nvim/lua/mongwell/treesitter/init.lua
+++
@@ -1,7 +0,0 @@
-local status_ok, _ = pcall(require, "nvim-treesitter")
-if not status_ok then
-    return
-end
-
-require("mongwell.treesitter.config")
-require("mongwell.treesitter.context")
