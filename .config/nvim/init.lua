9a0b9e341cd716be88c5b32bc2eb71debf05eb95
mongwell <19lion@gmail.com>
2023-07-31 20:35:49 -0500

    disable config change notifications

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,11 +3,16 @@ require("mongwell.functions")
 
 vim.g.colorscheme = "kanagawa"
 
+local lazy_opts = {
+    change_detection = {
+        notify = false,
+    }
+}
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 require("mongwell.bootstrap").lazy_setup(lazypath)
 local status_ok, lazy = pcall(require, "lazy")
 if status_ok then
-    lazy.setup("mongwell.plugins")
+    lazy.setup("mongwell.plugins", lazy_opts)
 end
 
 require("mongwell.keymaps")
