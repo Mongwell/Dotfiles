b2d1762af55543648332d350b12263d52b84d996
mongwell <19lion@gmail.com>
2023-07-31 21:17:44 -0500

    revert opt

--- .config/nvim/lua/mongwell/plugins/indentline.lua
+++ .config/nvim/lua/mongwell/plugins/indentline.lua
@@ -2,6 +2,7 @@ local indentline_opts = {
     show_current_context = true,
     -- show_current_context = false, turn off if using indentscope
     use_treesitter = true,
+    show_trailing_blankline_indent = false,
     filetype_exclude = { "help", "org*" },
 }
 
