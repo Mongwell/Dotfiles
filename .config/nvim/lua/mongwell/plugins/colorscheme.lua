e6d38656cc4e8b2173e75551fdf04279b0c94b30
mongwell <19lion@gmail.com>
2023-08-27 12:47:57 -0700

    new colorschemes

--- .config/nvim/lua/mongwell/plugins/colorscheme.lua
+++ .config/nvim/lua/mongwell/plugins/colorscheme.lua
@@ -9,17 +9,19 @@ local scheme_plugins = {
         name = "tokyonight",
         opts = { style = "night" }
     },
-    { plugin = "navarasu/onedark.nvim",       name = "onedark" },
-    { plugin = "marko-cerovac/material.nvim", name = "material", },
     { plugin = "sainnhe/gruvbox-material",    name = "gruvbox-material" },
-    { plugin = "glepnir/zephyr-nvim",         name = "zephyr" },
     { plugin = "sainnhe/sonokai",             name = "sonokai" },
+    { plugin = "sainnhe/everforest",          name = "everforest" },
+    { plugin = "rose-pine/neovim",            name = "rose-pine" },
+    { plugin = "catppuccin/nvim",             name = "catppuccin" },
+    { plugin = "edeneast/nightfox.nvim",      name = "nightfox" }
 }
 
 local M = {}
 for _, scheme in pairs(scheme_plugins) do
     table.insert(M, {
         scheme.plugin,
+        name = scheme.name,
         lazy = vim.g.colorscheme ~= scheme.name,
         priority = 1000,
         event = vim.g.colorscheme ~= scheme.name and nil or "VeryLazy",
