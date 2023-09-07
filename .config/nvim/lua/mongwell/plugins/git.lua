c65861680e2be3eb8d55f5620fa89d6f6b6db1f1
mongwell <19lion@gmail.com>
2023-09-06 20:38:45 -0700

    change gitsigns icons

--- .config/nvim/lua/mongwell/plugins/git.lua
+++ .config/nvim/lua/mongwell/plugins/git.lua
@@ -1,14 +1,16 @@
 local gitsigns_opts = {
     signs = {
-        add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-        change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-        delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-        topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-        changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "󰍵" },
+        topdelete = { text = "󰍵" },
+        changedelete = { text = "▎" },
+        -- delete = { text = "" },
+
     },
     signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-    numhl = false,  -- Toggle with `:Gitsigns toggle_numhl`
-    linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+    numhl = false,     -- Toggle with `:Gitsigns toggle_numhl`
+    linehl = false,    -- Toggle with `:Gitsigns toggle_linehl`
     word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
     watch_gitdir = {
         interval = 1000,
