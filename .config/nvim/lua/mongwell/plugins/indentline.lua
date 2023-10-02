8b7bfe7a5a2546646bb01aa13adbcdbfd4660e9a
mongwell <19lion@gmail.com>
2023-10-02 14:57:08 -0700

    update indentline config

--- .config/nvim/lua/mongwell/plugins/indentline.lua
+++ .config/nvim/lua/mongwell/plugins/indentline.lua
@@ -1,14 +1,17 @@
 local indentline_opts = {
-    show_current_context = true,
-    -- show_current_context = false, turn off if using indentscope
-    use_treesitter = true,
-    show_trailing_blankline_indent = false,
-    filetype_exclude = { "help", "org*" },
+    scope = {
+        enabled = false,
+    },
+    exclude = {
+        filetypes = { "help", "org*", "yaml", "yml" },
+    }
 }
 
 return {
     "lukas-reineke/indent-blankline.nvim",
+    dependencies = { "nvim-treesitter" },
     lazy = true,
     event = { "BufReadPost", "BufNewFile" },
     opts = indentline_opts,
+    main = "ibl",
 }
