216c02d8d30c619bc75981074876faeacef16efa
mongwell <19lion@gmail.com>
2025-07-21 20:37:41 -0700

    update deprecated function

--- .config/nvim/lua/mongwell/plugins/lualine/components.lua
+++ .config/nvim/lua/mongwell/plugins/lualine/components.lua
@@ -1,7 +1,7 @@
 M = {}
 
 local function servers()
-    local buf_clients = vim.lsp.get_active_clients({ bufnr = 0 })
+    local buf_clients = vim.lsp.get_clients({ bufnr = 0 })
     if #buf_clients == 0 then
         return ""
     end
