ced454e1898519efc31163d373aece77e99106a1
mongwell <19lion@gmail.com>
2025-09-28 20:53:54 -0700

    remove windows snippet build exception

--- .config/nvim/lua/mongwell/plugins/completion.lua
+++ .config/nvim/lua/mongwell/plugins/completion.lua
@@ -131,9 +131,7 @@ end
 return {
     {
         "L3MON4D3/LuaSnip",
-        build = (not jit.os:find("Windows"))
-            and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
-            or nil,
+        build = "make install_jsregexp",
         lazy = true,
         dependencies = {
             "rafamadriz/friendly-snippets",
