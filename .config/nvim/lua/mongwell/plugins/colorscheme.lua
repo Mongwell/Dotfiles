be534932e0c2bad3ffb9b90cbe11cf67239757f1
mongwell <19lion@gmail.com>
2025-09-28 19:15:44 -0700

    add some comments to colorscheme

--- .config/nvim/lua/mongwell/plugins/colorscheme.lua
+++ .config/nvim/lua/mongwell/plugins/colorscheme.lua
@@ -2,7 +2,16 @@ local scheme_plugins = {
     {
         plugin = "rebelot/kanagawa.nvim",
         name = "kanagawa",
-        opts = { colors = { theme = { all = { ui = { bg_gutter = "none" } } } } },
+        opts = {
+            colors = {
+                theme = {
+                    all = {
+                        -- remove background color of number column/sign gutter
+                        ui = { bg_gutter = "none" }
+                    }
+                }
+            }
+        },
     },
     {
         plugin = "folke/tokyonight.nvim",
@@ -21,12 +30,13 @@ local scheme_plugins = {
     { plugin = "edeneast/nightfox.nvim",   name = "nightfox" }
 }
 
+-- colorscheme config is pretty uniform, so we can auto generate it
 local M = {}
 for _, scheme in pairs(scheme_plugins) do
     table.insert(M, {
         scheme.plugin,
         name = scheme.name,
-        lazy = vim.g.colorscheme ~= scheme.name,
+        lazy = vim.g.colorscheme ~= scheme.name,  -- lazy load all schemes except the active one
         priority = 1000,
         event = vim.g.colorscheme ~= scheme.name and nil or "VeryLazy",
         opts = scheme.opts,
@@ -34,6 +44,9 @@ for _, scheme in pairs(scheme_plugins) do
             if scheme.opts then
                 require(scheme.name).setup(opts)
             end
+            -- vim.g.colorscheme is just a global var. vim.cmd.colorscheme()
+            -- will activate a colorscheme. manually calling it here allows
+            -- us to set options (like default flavor) first
             if vim.g.colorscheme == scheme.name then
                 vim.cmd.colorscheme(scheme.name)
             end
